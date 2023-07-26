19fec7abe3d5d8cb88fafcb6cfc12f630672d942
Mark Chweya <chweyamark@gmail.com>
2023-07-26 16:15:39 +0300

    My new branch with the staff model serializer cont

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,7 +1,7 @@
 class Staff < ApplicationRecord
 
 
-  
+
     belongs_to :restaurant
     has_secure_password
     has_many :order_tracks
@@ -9,7 +9,7 @@ class Staff < ApplicationRecord
     # Validations
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-
+   
 
 
   end
