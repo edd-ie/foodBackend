7de353c98a93c9d7e3703458cad2f6026b847862
Mark Chweya <chweyamark@gmail.com>
2023-07-26 19:18:45 +0300

    removed the unnecessary files

--- db/migrate/20230726160045_add_email_to_staff.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailToStaff < ActiveRecord::Migration[7.0]
-  def change
-    add_column :staffs, :email, :string
-    add_index :staffs, :email
-  end
-end
--- db/migrate/20230726160417_add_password_digest_to_staff.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordDigestToStaff < ActiveRecord::Migration[7.0]
-  def change
-    add_column :staffs, :password_digest, :string
-  end
-end
