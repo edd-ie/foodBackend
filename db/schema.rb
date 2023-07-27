e15b6596ef9ad9a64f6e253c23c8632e9aa55d9f
Mark Chweya <chweyamark@gmail.com>
2023-07-27 09:50:57 +0300

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -123,6 +123,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_165044) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.index ["email"], name: "index_staffs_on_email"
   end
 
 end
