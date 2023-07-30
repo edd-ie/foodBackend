bfa745339b44a85d0bb847cf09c2bb7dc8f3e215
edd-ie <ogendoeddie@gmail.com>
2023-07-30 13:11:50 +0300

    Bug fix

--- app/models/food_review.rb
+++ app/models/food_review.rb
@@ -2,7 +2,7 @@ class FoodReview < ApplicationRecord
     belongs :food
     belongs :customer
 
-    validates :comment, :food_id, :customer_id  presence: true
+    validates :comment, :food_id, :customer_id,  presence: true
     validates :likes, :dislikes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
 end
--- app/models/restaurant_review.rb
+++ app/models/restaurant_review.rb
@@ -2,6 +2,6 @@ class RestaurantReview < ApplicationRecord
     belongs :restaurant
     belongs :customer
 
-    validates :comment, :restaurant_id, :customer_id  presence: true
+    validates :comment, :restaurant_id, :customer_id,  presence: true
     validates :likes, :dislikes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
